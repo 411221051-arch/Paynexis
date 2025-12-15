35d088cb4996e101ed6efb20e211be2453106bba
Roman <vivalaboomxxx@gmail.com>
2025-12-15 15:44:34 +0300

    PrivateCabin_1512

---
+++ service/auth_service.go
@@ -0,0 +1,43 @@
+package service
+
+import (
+	"database/sql"
+	"errors"
+	"privateCabin/entity"
+	"privateCabin/repository"
+)
+
+type UserService interface {
+	GetUser(login, password string) (*entity.UserPublicDTO, error)
+	CreateUser(login, password string) (*entity.UserPublicDTO, error)
+}
+
+type userService struct {
+	repo repository.UserRepository
+}
+
+func NewUserService(r repository.UserRepository) UserService {
+	return &userService{repo: r}
+}
+
+func (s *userService) GetUser(login, password string) (*entity.UserPublicDTO, error) {
+	if login == "" || password == "" {
+		return nil, errors.New("login and password required")
+	}
+
+	user, err := s.repo.GetUserByLogin(login, password)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, errors.New("invalid login or password") // ← бизнес-ошибка (не технический ErrNoRows)
+		}
+		return nil, err // ← техническая ошибка БД
+	}
+	return user, nil
+}
+
+func (s *userService) CreateUser(login, password string) (*entity.UserPublicDTO, error) {
+	if login == "" || password == "" {
+		return nil, errors.New("login and password required")
+	}
+	return s.repo.CreateUserByData(login, password)
+}
